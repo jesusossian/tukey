94b227907f76f9f6a5c09033fee9d62f62826ac8
Jesus Ossian <jesusossian@gmail.com>
2023-05-15 22:28:50 +0100

    update after errors

---
+++ src/teste.py
@@ -0,0 +1,12 @@
+import urllib.request
+import io
+import zipfile
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+import gurobipy as gp
+from gurobipy import GRB
+
+import igraph as ig
